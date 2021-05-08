428e1e25d446d322a2cde98e25e12489fcb3ca2a
fea1436 <ehsan.aslany@yahoo.com>
2021-05-08 22:33:33 +0430

    ViewBags Applied But with Not Result

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
+
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
@@ -25,12 +26,20 @@ namespace DevCard_MVC.Controllers
             return View(model);
         }
 
-        //[HttpPost]
-        //public JsonResult Contact(Contact form)
-        //{
-        //    Console.WriteLine(form.ToString());
-        //    return Json(Ok());
-        //}
+        [HttpPost]
+        public IActionResult Contact(Contact form)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "ایراد در ورود اطلاعات! مجدداً تلاش کنید";
+                ViewBag.success = "";
+                return View(form);
+            }
+
+            ViewBag.error = "";
+            ViewBag.success = "اطلاعات با موفقیت به سرور ارسال گردید";
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
